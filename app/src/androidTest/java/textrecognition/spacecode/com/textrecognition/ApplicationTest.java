5dd7c3d0559e7d38d593030daa3b85ab27b58478
amr hussien <amr hussien>
2017-03-07 15:03:00 +0200

    Initial commit

---
+++ app/src/androidTest/java/textrecognition/spacecode/com/textrecognition/ApplicationTest.java
@@ -0,0 +1,13 @@
+package textrecognition.spacecode.com.textrecognition;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
